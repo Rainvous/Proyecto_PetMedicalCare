bfb6d9ab8c4bac40760258ae6e42947f20bcd0c9
alvarofabrizio <ferrodominguezalvaro@gmail.com>
2025-09-15 15:52:00 -0500

    CambioDePrintf
    He cambiado el mensaje

--- PetMedicalCareV1/SoftPetDBManager/src/main/java/pe/edu/pucp/softpetdbmanager/SoftPetDBManager.java
+++ PetMedicalCareV1/SoftPetDBManager/src/main/java/pe/edu/pucp/softpetdbmanager/SoftPetDBManager.java
@@ -11,6 +11,6 @@ package pe.edu.pucp.softpetdbmanager;
 public class SoftPetDBManager {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Amaru");
     }
 }
