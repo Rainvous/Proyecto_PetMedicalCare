0bc08653252cc4d489c509cffce048d0f4244137
DavidSQ114 <david.santana.quispe114@gmail.com>
2025-09-15 19:39:11 -0500

    PROYECTO MODEL
    CREACION DEL PROYECTO MODEL

---
+++ PetMedicalCareV1/SoftPetModel/src/main/java/pe/edu/pucp/softpet/model/MascotasDTO.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.softpet.model;
+
+/**
+ *
+ * @author snipe
+ */
+public class MascotasDTO {
+    
+    private Integer mascotaId;
+    private PersonasDTO personaId;
+    private String nombre;
+    private String raza;
+    private String color;
+
+    public MascotasDTO() {
+        this.mascotaId = null;
+        this.personaId = null;
+        this.nombre = null;
+        this.raza = null;
+        this.color = null;
+    }    
+    
+    public MascotasDTO(Integer mascotaId, PersonasDTO personaId, String nombre, String raza, String color) {
+        this.mascotaId = mascotaId;
+        this.personaId = personaId;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.color = color;
+    }
+
+    /**
+     * @return the mascotaId
+     */
+    public Integer getMascotaId() {
+        return mascotaId;
+    }
+
+    /**
+     * @param mascotaId the mascotaId to set
+     */
+    public void setMascotaId(Integer mascotaId) {
+        this.mascotaId = mascotaId;
+    }
+
+    /**
+     * @return the personaId
+     */
+    public PersonasDTO getPersonaId() {
+        return personaId;
+    }
+
+    /**
+     * @param personaId the personaId to set
+     */
+    public void setPersonaId(PersonasDTO personaId) {
+        this.personaId = personaId;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the raza
+     */
+    public String getRaza() {
+        return raza;
+    }
+
+    /**
+     * @param raza the raza to set
+     */
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+}
---
+++ PetMedicalCareV1/SoftPetModel/src/main/java/pe/edu/pucp/softpet/model/VeterinariosDTO.java
@@ -0,0 +1,126 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.softpet.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author snipe
+ */
+public class VeterinariosDTO {
+    
+    private Integer veterinarioId;
+    private String especializacion;
+    private Date fechaDeContratacion;
+    private PersonasDTO personaId;
+    private String estado;
+    private Double sueldo;
+
+    public VeterinariosDTO() {
+        this.veterinarioId = null;
+        this.especializacion = null;
+        this.fechaDeContratacion = null;
+        this.personaId = null;
+        this.estado = null;
+        this.sueldo = null;
+    }
+    
+    public VeterinariosDTO(Integer veterinarioId, String especializacion, Date fechaDeContratacion, PersonasDTO personaId, String estado, Double sueldo) {
+        this.veterinarioId = veterinarioId;
+        this.especializacion = especializacion;
+        this.fechaDeContratacion = fechaDeContratacion;
+        this.personaId = personaId;
+        this.estado = estado;
+        this.sueldo = sueldo;
+    }
+
+    /**
+     * @return the veterinarioId
+     */
+    public Integer getVeterinarioId() {
+        return veterinarioId;
+    }
+
+    /**
+     * @param veterinarioId the veterinarioId to set
+     */
+    public void setVeterinarioId(Integer veterinarioId) {
+        this.veterinarioId = veterinarioId;
+    }
+
+    /**
+     * @return the especializacion
+     */
+    public String getEspecializacion() {
+        return especializacion;
+    }
+
+    /**
+     * @param especializacion the especializacion to set
+     */
+    public void setEspecializacion(String especializacion) {
+        this.especializacion = especializacion;
+    }
+
+    /**
+     * @return the fechaDeContratacion
+     */
+    public Date getFechaDeContratacion() {
+        return fechaDeContratacion;
+    }
+
+    /**
+     * @param fechaDeContratacion the fechaDeContratacion to set
+     */
+    public void setFechaDeContratacion(Date fechaDeContratacion) {
+        this.fechaDeContratacion = fechaDeContratacion;
+    }
+
+    /**
+     * @return the personaId
+     */
+    public PersonasDTO getPersonaId() {
+        return personaId;
+    }
+
+    /**
+     * @param personaId the personaId to set
+     */
+    public void setPersonaId(PersonasDTO personaId) {
+        this.personaId = personaId;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the sueldo
+     */
+    public Double getSueldo() {
+        return sueldo;
+    }
+
+    /**
+     * @param sueldo the sueldo to set
+     */
+    public void setSueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    
+    
+}
