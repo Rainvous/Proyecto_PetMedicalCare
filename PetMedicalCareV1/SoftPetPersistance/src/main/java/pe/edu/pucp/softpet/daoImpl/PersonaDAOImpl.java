b9f1cd84bd15ae78f1b175ac919eeb8ce7f519e2
Rainvous <amaru.manuel.varillas@gmail.com>
2025-09-15 22:58:24 -0500

    AcabandoImplPersona

--- PetMedicalCareV1/SoftPetPersistance/src/main/java/pe/edu/pucp/softpet/daoImpl/PersonaDAOImpl.java
+++ PetMedicalCareV1/SoftPetPersistance/src/main/java/pe/edu/pucp/softpet/daoImpl/PersonaDAOImpl.java
@@ -2,66 +2,296 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+//package pe.edu.pucp.softpet.daoImpl;
+//
+//import java.sql.SQLException;
+//import java.util.ArrayList;
+//import static javax.management.Query.or;
+//import pe.edu.pucp.softpet.daoImpl.base.DAOImplBase;
+//import pe.edu.pucp.softpet.model.actoresdto.PersonasDTO;
+//import pe.edu.pucp.softpet.dao.PersonaDAO;
+//import pe.edu.pucp.softpet.daoImpl.util.Columna;
+//import pe.edu.pucp.softpet.model.TipoSexo;
+//
+///**
+// *
+// * @author AMARU 
+//NOTA: ESTE CODIGO LO DEJO POR SI ACASO
+
+// */
+//public class PersonaDAOImpl extends DAOImplBase implements PersonaDAO {
+//
+//    private PersonasDTO persona;
+//
+//    public PersonaDAOImpl(String nombre_tabla) {
+//        super("PERSONA");
+//        this.persona = null;
+//    }
+//
+//    @Override
+//    protected void configurarListaDeColumnas() {
+//
+//        this.listaColumnas.add(new Columna("PERSONA_ID", true, true));
+//        this.listaColumnas.add(new Columna("NOMBRE", false, false));
+//        this.listaColumnas.add(new Columna("DIRECCION", false, false));
+//        this.listaColumnas.add(new Columna("CORREO", false, false));
+//
+//        this.listaColumnas.add(new Columna("TELEFONO", false, false));
+//
+//        this.listaColumnas.add(new Columna("SEXO", false, false));
+//        this.listaColumnas.add(new Columna("TIPO_PERSONA", false, false));
+//        this.listaColumnas.add(new Columna("TIPO_DOCUMENTO", false, false));
+//        this.listaColumnas.add(new Columna("NRO_DOCUMENTO", false, false));
+//        // new Columna(nombre_tabla, retornarLlavePrimaria, retornarLlavePrimaria)
+//    }
+//
+//    @Override
+//    public Integer insertar(PersonasDTO persona) {
+//        this.persona = persona;
+//        return super.insertar();
+//    }
+//
+//    @Override
+//    public PersonasDTO obtenerPorId(Integer personaId) {
+//        // Preparar “contexto” para que los hooks sepan qué ID poner en el WHERE
+//        this.persona = new PersonasDTO();
+//        this.persona.setPersonaId(personaId);
+//
+//        super.obtenerPorId(); // DAOImplBase ejecuta SELECT ... WHERE PK=?, y luego llama a instanciarObjetoDelResultSet()
+//
+//        return this.persona; // queda seteada en instanciarObjetoDelResultSet()
+//    }
+//    //INICIO DE FUNCIONES DE LA CLASE DAOImplBase
+//
+//    @Override
+//    protected void incluirValorDeParametrosParaInsercion() throws SQLException {
+//        Integer condicion;
+//        this.statement.setString(1, this.persona.getNombre());
+//        this.statement.setString(2, this.persona.getDireccion());
+//        this.statement.setString(3, this.persona.getCorreo());
+//        this.statement.setString(4, this.persona.getTelefono());
+//
+//        this.statement.setInt(5, this.persona.getSexo() == TipoSexo.FEMENINO ? 1 : 0);
+//        //CASO DE ARRIBA ES PARA ENUM
+//        this.statement.setString(6, this.persona.getTipoPersona());
+//        this.statement.setString(7, this.persona.getTipoDocumento());
+//        this.statement.setString(7, this.persona.getNroDocumento());
+//    }
+//
+//    @Override
+//    protected void incluirValorDeParametrosParaModificacion() throws SQLException {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+//
+//    @Override
+//    protected void incluirValorDeParametrosParaEliminacion() throws SQLException {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+//    //FIN DE LA FUNCIONES DAOImplBase
+//
+//    @Override
+//    public ArrayList<PersonasDTO> listarTodos() {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+//
+//    @Override
+//    public Integer modificar(PersonasDTO persona) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+//
+//    @Override
+//    public Integer eliminar(PersonasDTO persona) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+//
+//}
 package pe.edu.pucp.softpet.daoImpl;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
-import pe.edu.pucp.softpet.daoImpl.base.DAOImplBase;
-import pe.edu.pucp.softpet.model.actoresdto.PersonasDTO;
-import pe.edu.pucp.softpet.dao.PersonaDAO;
-import pe.edu.pucp.softpet.daoImpl.util.Columna;
+import java.util.List;
 
-/**
- *
- * @author User
- */
+import pe.edu.pucp.softpet.dao.PersonaDAO;
+import pe.edu.pucp.softpet.daoImpl.base.DAOImplBase;
+import pe.edu.pucp.softpet.daoImpl.util.Columna;
+import pe.edu.pucp.softpet.model.TipoSexo;
+import pe.edu.pucp.softpet.model.actoresdto.PersonasDTO;
+
 public class PersonaDAOImpl extends DAOImplBase implements PersonaDAO {
 
     private PersonasDTO persona;
 
-    public PersonaDAOImpl(String nombre_tabla) {
-        super("PERSONA");
-        this.persona=null;
+    public PersonaDAOImpl() {
+        super("PERSONA"); // nombre de tabla en BD
+        this.persona = null;
     }
-    
-    @Override
-    protected void configurarListaDeColumnas() {
-        
-        this.listaColumnas.add(new Columna("PERSONA_ID", true, true));
-        this.listaColumnas.add(new Columna("NOMBRE", false, false));
-        this.listaColumnas.add(new Columna("DIRECCION",false,false));
-        this.listaColumnas.add(new Columna("CORREO",false,false));
-        
-        this.listaColumnas.add(new Columna("TELEFONO",false,false));
-        
-        this.listaColumnas.add(new Columna("SEXO",false,false));
-        this.listaColumnas.add(new Columna("TIPO_PERSONA",false,false));
-        this.listaColumnas.add(new Columna("TIPO_DOCUMENTO",false,false));
-        this.listaColumnas.add(new Columna("NRO_DOCUMENTO",false,false));
+
+    public PersonaDAOImpl(String nombre_tabla) {
+        super(nombre_tabla);
+        this.persona = null;
     }
 
     @Override
+    protected void configurarListaDeColumnas() {
+        // Definir columnas y metadatos (PK, autogenerado)
+        // Orden aquí importa: se ocupará para generar SQL y parámetros.
+        this.listaColumnas.add(new Columna("PERSONA_ID", true,  true));  // PK, autogenerado
+        this.listaColumnas.add(new Columna("NOMBRE",      false, false));
+        this.listaColumnas.add(new Columna("DIRECCION",   false, false));
+        this.listaColumnas.add(new Columna("CORREO",      false, false));
+        this.listaColumnas.add(new Columna("TELEFONO",    false, false));
+        this.listaColumnas.add(new Columna("SEXO",        false, false));
+        this.listaColumnas.add(new Columna("TIPO_PERSONA",false, false));
+        this.listaColumnas.add(new Columna("TIPO_DOCUMENTO", false, false));
+        this.listaColumnas.add(new Columna("NRO_DOCUMENTO",  false, false));
+    }
+
+    // ===== PersonaDAO =====
+
+    @Override
     public Integer insertar(PersonasDTO persona) {
-        
+        this.persona = persona;
+        // Si quieres que retorne el ID autogenerado:
+        this.retornarLlavePrimaria = true;
+        return super.insertar();
     }
 
     @Override
     public PersonasDTO obtenerPorId(Integer personaId) {
-        
+        // Preparar “contexto” para que los hooks sepan qué ID poner en el WHERE
+        this.persona = new PersonasDTO();
+        this.persona.setPersonaId(personaId);
+
+        super.obtenerPorId(); // DAOImplBase ejecuta SELECT ... WHERE PK=?, y luego llama a instanciarObjetoDelResultSet()
+
+        return this.persona; // queda seteada en instanciarObjetoDelResultSet()
     }
 
     @Override
     public ArrayList<PersonasDTO> listarTodos() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List lista = super.listarTodos(); // DAOImplBase iterará el ResultSet y llamará a agregarObjetoALaLista(lista)
+        ArrayList<PersonasDTO> resultado = new ArrayList<>();
+        for (Object o : lista) {
+            resultado.add((PersonasDTO) o);
+        }
+        return resultado;
     }
 
     @Override
     public Integer modificar(PersonasDTO persona) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        this.persona = persona;
+        return super.modificar();
     }
 
     @Override
     public Integer eliminar(PersonasDTO persona) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        this.persona = persona;
+        return super.eliminar();
     }
-    
+
+    // ===== Hooks llamados por DAOImplBase =====
+
+    @Override
+    protected void incluirValorDeParametrosParaInsercion() throws SQLException {
+        // Orden de parámetros = orden de columnas NO autogeneradas:
+        // NOMBRE, DIRECCION, CORREO, TELEFONO, SEXO, TIPO_PERSONA, TIPO_DOCUMENTO, NRO_DOCUMENTO
+        this.statement.setString(1, this.persona.getNombre());
+        this.statement.setString(2, this.persona.getDireccion());
+        this.statement.setString(3, this.persona.getCorreo());
+        this.statement.setString(4, this.persona.getTelefono());
+
+        // Si tu columna SEXO es VARCHAR:
+        this.statement.setString(5, this.persona.getSexo() != null ? this.persona.getSexo().name() : null);
+        // Si en tu BD SEXO es tinyint/number, cambia a:
+        // this.statement.setInt(5, this.persona.getSexo() == TipoSexo.FEMENINO ? 1 : 0);
+
+        this.statement.setString(6, this.persona.getTipoPersona());
+        this.statement.setString(7, this.persona.getTipoDocumento());
+        this.statement.setString(8, this.persona.getNroDocumento());
+    }
+
+    @Override
+    protected void incluirValorDeParametrosParaModificacion() throws SQLException {
+        // UPDATE PERSONA SET (todas NO PK) WHERE (PK)
+        // Mismo orden que inserción, y al final el PK:
+        this.statement.setString(1, this.persona.getNombre());
+        this.statement.setString(2, this.persona.getDireccion());
+        this.statement.setString(3, this.persona.getCorreo());
+        this.statement.setString(4, this.persona.getTelefono());
+        this.statement.setString(5, this.persona.getSexo() != null ? this.persona.getSexo().name() : null);
+        this.statement.setString(6, this.persona.getTipoPersona());
+        this.statement.setString(7, this.persona.getTipoDocumento());
+        this.statement.setString(8, this.persona.getNroDocumento());
+
+        // WHERE PERSONA_ID = ?
+        this.statement.setInt(9, this.persona.getPersonaId());
+    }
+
+    @Override
+    protected void incluirValorDeParametrosParaEliminacion() throws SQLException {
+        // DELETE ... WHERE PERSONA_ID = ?
+        this.statement.setInt(1, this.persona.getPersonaId());
+    }
+
+    @Override
+    protected void incluirValorDeParametrosParaObtenerPorId() throws SQLException {
+        // SELECT ... WHERE PERSONA_ID = ?
+        this.statement.setInt(1, this.persona.getPersonaId());
+    }
+
+    @Override
+    protected void instanciarObjetoDelResultSet() throws SQLException {
+        PersonasDTO p = new PersonasDTO();
+        p.setPersonaId(this.resultSet.getInt("PERSONA_ID"));
+        p.setNombre(this.resultSet.getString("NOMBRE"));
+        p.setDireccion(this.resultSet.getString("DIRECCION"));
+        p.setCorreo(this.resultSet.getString("CORREO"));
+        p.setTelefono(this.resultSet.getString("TELEFONO"));
+
+        // SEXO: si la BD guarda texto (FEMENINO/MASCULINO):
+        String sexoStr = this.resultSet.getString("SEXO");
+        if (sexoStr != null) {
+            try {
+                p.setSexo(TipoSexo.valueOf(sexoStr));
+            } catch (IllegalArgumentException ex) {
+                // Si guardan “F/M” u otros valores, aquí puedes mapear
+                if ("F".equalsIgnoreCase(sexoStr)) p.setSexo(TipoSexo.FEMENINO);
+                else if ("M".equalsIgnoreCase(sexoStr)) p.setSexo(TipoSexo.MASCULINO);
+                else p.setSexo(null);
+            }
+        }
+
+        p.setTipoPersona(this.resultSet.getString("TIPO_PERSONA"));
+        p.setTipoDocumento(this.resultSet.getString("TIPO_DOCUMENTO"));
+        p.setNroDocumento(this.resultSet.getString("NRO_DOCUMENTO"));
+
+        this.persona = p; // importante: `obtenerPorId` devolverá `this.persona`
+    }
+
+    @Override
+    protected void agregarObjetoALaLista(List lista) throws SQLException {
+        PersonasDTO p = new PersonasDTO();
+        p.setPersonaId(this.resultSet.getInt("PERSONA_ID"));
+        p.setNombre(this.resultSet.getString("NOMBRE"));
+        p.setDireccion(this.resultSet.getString("DIRECCION"));
+        p.setCorreo(this.resultSet.getString("CORREO"));
+        p.setTelefono(this.resultSet.getString("TELEFONO"));
+
+        String sexoStr = this.resultSet.getString("SEXO");
+        if (sexoStr != null) {
+            try {
+                p.setSexo(TipoSexo.valueOf(sexoStr));
+            } catch (IllegalArgumentException ex) {
+                if ("F".equalsIgnoreCase(sexoStr)) p.setSexo(TipoSexo.FEMENINO);
+                else if ("M".equalsIgnoreCase(sexoStr)) p.setSexo(TipoSexo.MASCULINO);
+                else p.setSexo(null);
+            }
+        }
+
+        p.setTipoPersona(this.resultSet.getString("TIPO_PERSONA"));
+        p.setTipoDocumento(this.resultSet.getString("TIPO_DOCUMENTO"));
+        p.setNroDocumento(this.resultSet.getString("NRO_DOCUMENTO"));
+
+        lista.add(p);
+    }
 }
