87f5925542bbc0a4931b3f9d4922f2c2bd771f05
Adrianna39 <adrianna.lopez@pucp.edu.pe>
2025-09-15 20:10:19 -0500

    Update Prueba.java

--- EntornoPruebas/prueba/src/main/java/pe/edu/pucp/prueba/Prueba.java
+++ EntornoPruebas/prueba/src/main/java/pe/edu/pucp/prueba/Prueba.java
@@ -12,6 +12,8 @@ public class Prueba {
 
     public static void main(String[] args) {
         System.out.println("Hola, soy Adri:)");
+        System.out.println("Hola, soy Adri:)");
+        System.out.println("Hola, soy Adri2:)");
    
     }
 }
