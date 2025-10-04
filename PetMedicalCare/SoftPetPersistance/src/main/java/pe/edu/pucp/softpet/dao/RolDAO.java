3a06c4ccc813e83de822eaf929022c8e0b63ef8a
alvarofabrizio <ferrodominguezalvaro@gmail.com>
2025-10-04 17:58:39 -0500

    RolDao

--- PetMedicalCare/SoftPetPersistance/src/main/java/pe/edu/pucp/softpet/dao/RolDAO.java
+++ PetMedicalCare/SoftPetPersistance/src/main/java/pe/edu/pucp/softpet/dao/RolDAO.java
@@ -1,28 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pe.edu.pucp.softpet.dao;
 
 import java.util.ArrayList;
 import pe.edu.pucp.softpet.dto.usuarios.RolDto;
 
-
-/**
- *
- * @author ferro
- */
 public interface RolDAO {
-    
+
     public Integer insertar(RolDto rol);
-    
+
     public RolDto obtenerPorId(Integer rolId);
-    
+
     public ArrayList<RolDto> listarTodos();
-    
+
     public Integer modificar(RolDto rol);
-    
+
     public Integer eliminar(RolDto rol);
-    
-    
 }
