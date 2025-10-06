5d0c1d6e6c72cb5a398a6fd16cd5ad2fbe3e44e2
Rainvous <amaru.manuel.varillas@gmail.com>
2025-10-06 18:56:02 -0500

    CambiosPersistance2

---
+++ PetMedicalCare/SoftPetPersistance/src/test/java/pe/edu/pucp/softpet/dao/DetalleRecetaDaoTest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package pe.edu.pucp.softpet.dao;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author User
+ */
+public class DetalleRecetaDaoTest {
+    
+//    public DetalleRecetaDaoTest() {
+//    }
+//
+//    @Test
+//    public void testSomeMethod() {
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+}
