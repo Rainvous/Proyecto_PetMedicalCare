28e78c205b37b867536702fb97349ef1cf54fa8a
Rainvous <amaru.manuel.varillas@gmail.com>
2025-10-07 23:07:17 -0500

    agua de aguas

--- SoftPruebas/SoftPetPersistance/src/main/java/pe/edu/pucp/softpet/dao/RolDao.java
+++ SoftPruebas/SoftPetPersistance/src/main/java/pe/edu/pucp/softpet/dao/RolDao.java
@@ -1,7 +1,8 @@
 package pe.edu.pucp.softpet.dao;
 
+import java.util.ArrayList;
 import pe.edu.pucp.softpet.dto.usuarios.RolDto;
 
 public interface RolDao extends DaoBase<RolDto> {
-    
+    ArrayList<RolDto> ObtenerRolesDelUsuario(Integer idUser);
 }
