cce6c78df54f4e7cfff544bd4883428b249f93e8
alvarofabrizio <ferrodominguezalvaro@gmail.com>
2025-10-28 23:02:11 -0500

    Persi-Check

--- PetMedicalCare/SoftPetPersistance/src/main/java/pe/edu/pucp/softpet/dao/TipoDocumentoDao.java
+++
@@ -1,7 +0,0 @@
-//package pe.edu.pucp.softpet.dao;
-//
-//import pe.edu.pucp.softpet.dto.facturacion.TipoDocumentoDto;
-//
-//public interface TipoDocumentoDao extends DaoBase<TipoDocumentoDto> {
-//
-//}
