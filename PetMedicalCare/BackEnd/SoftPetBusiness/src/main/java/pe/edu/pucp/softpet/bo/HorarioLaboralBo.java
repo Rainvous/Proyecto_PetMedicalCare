2ae76850da663bafdb3f2bb76e2eca262f0fc6db
MartinVsq <a20222615@pucp.edu.pe>
2025-11-15 13:11:00 -0500

    Cambiaso

---
+++ PetMedicalCare/BackEnd/SoftPetBusiness/src/main/java/pe/edu/pucp/softpet/bo/HorarioLaboralBo.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.softpet.bo;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import pe.edu.pucp.softpet.dao.HorarioLaboralDao;
+import pe.edu.pucp.softpet.daoImp.HorarioLaboralDaoImpl;
+import pe.edu.pucp.softpet.dto.personas.HorarioLaboralDto;
+import pe.edu.pucp.softpet.dto.personas.VeterinarioDto;
+import pe.edu.pucp.softpet.dto.util.enums.EstadoLaboral;
+
+public class HorarioLaboralBo {
+
+    private final HorarioLaboralDao horarioDao;
+
+    public HorarioLaboralBo() {
+        this.horarioDao = new HorarioLaboralDaoImpl();
+    }
+
+    public Integer insertar(int veterinarioId, Date fecha, EstadoLaboral estado,
+            Date horaInicio, Date horaFin, boolean activo) {
+
+        HorarioLaboralDto horarioDto = new HorarioLaboralDto();
+        
+        VeterinarioDto veterinario = new VeterinarioDto();
+        veterinario.setVeterinarioId(veterinarioId);
+        
+        horarioDto.setVeterinario(veterinario);
+        horarioDto.setFecha(fecha);
+        horarioDto.setEstado(estado);
+        horarioDto.setHoraInicio(horaInicio);
+        horarioDto.setHoraFin(horaFin);
+        horarioDto.setActivo(activo);
+
+        return this.horarioDao.insertar(horarioDto);
+    }
+
+    public Integer modificar(int hotarioId, int veterinarioId, Date fecha, EstadoLaboral estado,
+            Date horaInicio, Date horaFin, boolean activo) {
+
+        HorarioLaboralDto horarioDto = new HorarioLaboralDto();
+
+        VeterinarioDto veterinario = new VeterinarioDto();
+        veterinario.setVeterinarioId(veterinarioId);
+        
+        horarioDto.setHorarioLaboralId(hotarioId);
+        horarioDto.setVeterinario(veterinario);
+        horarioDto.setFecha(fecha);
+        horarioDto.setEstado(estado);
+        horarioDto.setHoraInicio(horaInicio);
+        horarioDto.setHoraFin(horaFin);
+        horarioDto.setActivo(activo);
+
+        return this.horarioDao.modificar(horarioDto);
+    }
+
+    public Integer eliminar(Integer horarioId) {
+        HorarioLaboralDto horarioDto = new HorarioLaboralDto();
+        horarioDto.setHorarioLaboralId(horarioId);
+        return this.horarioDao.eliminar(horarioDto);
+    }
+
+    public HorarioLaboralDto obtenerPorId(Integer horarioId) {
+        HorarioLaboralDto horarioDto = new HorarioLaboralDto();
+        horarioDto.setHorarioLaboralId(horarioId);
+        return this.horarioDao.obtenerPorId(horarioId);
+    }
+
+    public ArrayList<HorarioLaboralDto> listarTodos() {
+        return this.horarioDao.listarTodos();
+    }
+}
---
+++ PetMedicalCare/BackEnd/SoftPetWS/src/main/java/pe/edu/pucp/softpetws/horariolaboral/HorarioLaboral.java
@@ -0,0 +1,144 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.softpetws.horariolaboral;
+
+/**
+ *
+ * @author marti
+ */
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebService;
+import jakarta.xml.ws.WebServiceException;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import pe.edu.pucp.softpet.bo.HorarioLaboralBo;
+import pe.edu.pucp.softpet.dto.personas.HorarioLaboralDto;
+import pe.edu.pucp.softpet.dto.util.enums.EstadoLaboral;
+
+@WebService(serviceName = "Horarios")
+public class HorarioLaboral {
+
+    private final HorarioLaboralBo horarioBo = new HorarioLaboralBo();
+
+    // =========================
+    //       INSERTAR
+    // =========================
+    @WebMethod(operationName = "insertar_horario")
+    public Integer insertar(
+            @WebParam(name = "veterinarioId") int veterinarioId,
+            @WebParam(name = "fecha") String fecha,
+            @WebParam(name = "estado") String estado,
+            @WebParam(name = "horaInicio") String horaInicio,
+            @WebParam(name = "horaFin") String horaFin,
+            @WebParam(name = "activo") boolean activo) {
+
+        try {
+//            SimpleDateFormat dfFecha = new SimpleDateFormat("yyyyMMdd");
+//            java.util.Date utilFecha = dfFecha.parse(fechaRegistro);
+
+            // yyyyMMdd
+            long millis = System.currentTimeMillis();
+            Date fechaSql = new Date(millis);
+            Date fechaHoraInicioSql = parseFechaHora(horaInicio); // yyyyMMddHHmmss
+            Date fechaHoraFinSql = parseFechaHora(horaFin);       // yyyyMMddHHmmss
+            EstadoLaboral estadoEnum = parseEstado(estado);
+
+            return horarioBo.insertar(
+                    veterinarioId,
+                    fechaSql,
+                    estadoEnum,
+                    fechaHoraInicioSql,
+                    fechaHoraFinSql,
+                    activo
+            );
+
+        } catch (ParseException e) {
+            throw new WebServiceException("Error al convertir fechas: " + e.getMessage(), e);
+        } catch (IllegalArgumentException e) {
+            throw new WebServiceException(
+                    "Estado de cita inválido (PROGRAMADA, ATENDIDA, CANCELADA): " + estado, e);
+        }
+    }
+
+    // =========================
+    //       MODIFICAR
+    // =========================
+    @WebMethod(operationName = "modificar_horario")
+    public Integer modificar(@WebParam(name = "horarioId") int horarioId,
+            @WebParam(name = "veterinarioId") int veterinarioId,
+            @WebParam(name = "fecha") String fecha,
+            @WebParam(name = "estado") String estado,
+            @WebParam(name = "horaInicio") String horaInicio,
+            @WebParam(name = "horaFin") String horaFin,
+            @WebParam(name = "activo") boolean activo) {
+
+        try {
+//            
+// yyyyMMdd
+            long millis = System.currentTimeMillis();
+            Date fechaSql = new Date(millis);
+            Date fechaHoraInicioSql = parseFechaHora(horaInicio); // yyyyMMddHHmmss
+            Date fechaHoraFinSql = parseFechaHora(horaFin);       // yyyyMMddHHmmss
+            EstadoLaboral estadoEnum = parseEstado(estado);
+
+            return horarioBo.modificar(horarioId,
+                    veterinarioId,
+                    fechaSql,
+                    estadoEnum,
+                    fechaHoraInicioSql,
+                    fechaHoraFinSql,
+                    activo
+            );
+
+        } catch (ParseException e) {
+            throw new WebServiceException("Error al convertir fechas: " + e.getMessage(), e);
+        } catch (IllegalArgumentException e) {
+            throw new WebServiceException(
+                    "Estado de cita inválido (PROGRAMADA, ATENDIDA, CANCELADA): " + estado, e);
+        }
+    }
+
+    // =========================
+    //   OBTENER POR ID
+    // =========================
+    @WebMethod(operationName = "obtener_por_id")
+    public HorarioLaboralDto obtenerPorId(
+            @WebParam(name = "citaAtencionId") int horarioId) {
+
+        return this.horarioBo.obtenerPorId(horarioId);
+    }
+
+    // =========================
+    //    LISTAR TODOS
+    // =========================
+    @WebMethod(operationName = "listar_todos")
+    public ArrayList<HorarioLaboralDto> listarTodos() {
+        return this.horarioBo.listarTodos();
+    }
+
+    // =========================
+    //   MÉTODOS PRIVADOS DE APOYO
+    // =========================
+    private Date parseFecha(String fechaStr) throws ParseException {
+        // Formato esperado: yyyyMMdd  -> 20251210
+        SimpleDateFormat dfFecha = new SimpleDateFormat("yyyyMMdd");
+        java.util.Date utilFecha = dfFecha.parse(fechaStr);
+        return new Date(utilFecha.getTime());
+    }
+
+    private Date parseFechaHora(String fechaHoraStr) throws ParseException {
+        // Formato esperado: yyyyMMddHHmmss -> 20250911245020
+        SimpleDateFormat dfFechaHora = new SimpleDateFormat("yyyyMMddHHmmss");
+        java.util.Date utilFechaHora = dfFechaHora.parse(fechaHoraStr);
+        return new Date(utilFechaHora.getTime());
+    }
+
+    private EstadoLaboral parseEstado(String estadoStr) {
+        return EstadoLaboral.valueOf(estadoStr);
+    }
+}
